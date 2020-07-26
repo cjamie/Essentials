eb05ffcbf7d56faa34daff17de60099431ef4188
Jamie <jamiechu9288@gmail.com>
2020-07-26 01:16:32 -0400

    create personal tag along project

---
+++ MyEssentialFeed/MyEssentialFeed/MyEssentialFeed.h
@@ -0,0 +1,19 @@
+//
+//  MyEssentialFeed.h
+//  MyEssentialFeed
+//
+//  Created by Jamie Chu on 7/26/20.
+//  Copyright © 2020 Jamie Chu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyEssentialFeed.
+FOUNDATION_EXPORT double MyEssentialFeedVersionNumber;
+
+//! Project version string for MyEssentialFeed.
+FOUNDATION_EXPORT const unsigned char MyEssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyEssentialFeed/PublicHeader.h>
+
+
