b422a0a8134f462d9ee3d618b65aa2faebf5b05f
Jamie <jamiechu9288@gmail.com>
2020-07-26 01:17:06 -0400

    create feed item model

---
+++ MyEssentialFeed/MyEssentialFeed/MyFeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  MyFeedItem.swift
+//  MyEssentialFeed
+//
+//  Created by Jamie Chu on 7/26/20.
+//  Copyright © 2020 Jamie Chu. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let url: URL
+}
