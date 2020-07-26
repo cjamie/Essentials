9fd00da7373c5dff785ada162b02871529694b46
Jamie <jamiechu9288@gmail.com>
2020-07-26 02:08:00 -0400

    changed httpclient into a protocol, changed implmentation of remotefeed loader to take in an extra url parameter

--- MyEssentialFeed/MyEssentialFeedTests/RemoteFeedLoaderTests.swift
+++ MyEssentialFeed/MyEssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -11,39 +11,49 @@ import XCTest
 final class RemoteFeedLoader {
 
     private let client: HTTPClient
-    
+    private let url: URL
     // MARK: - Init
     
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
         self.client = client
+        self.url = url
     }
     
     func load() {
-        client.requestedURL = URL(string: "https://a-url.com")
+        client.get(from: url)
     }
 
 }
 
-final class HTTPClient {
-    
+protocol HTTPClient {
+    func get(from: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
-        _ = RemoteFeedLoader(client: client)
+        let client = HTTPClientSpy()
+        let url = URL(string: "https://a-url.com")!
+        _ = RemoteFeedLoader(url: url, client: client)
 
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClient()
-        let sut = RemoteFeedLoader(client: client)
+        let client = HTTPClientSpy()
+        let url = URL(string: "https://a-url.com")!
+        let sut = RemoteFeedLoader(url: url, client: client)
         
         sut.load()
         
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
