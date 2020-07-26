83b5c0a10fbfc6d6d40505c640a85e428a7b6d1e
Jamie <jamiechu9288@gmail.com>
2020-07-26 01:38:03 -0400

    added failing test, configured testing scheme with MyEssentailFeed scheme

---
+++ MyEssentialFeed/MyEssentialFeedTests/MyEssentialFeedTests.swift
@@ -0,0 +1,19 @@
+//
+//  MyEssentialFeedTests.swift
+//  MyEssentialFeedTests
+//
+//  Created by Jamie Chu on 7/26/20.
+//  Copyright © 2020 Jamie Chu. All rights reserved.
+//
+
+import XCTest
+
+class MyEssentialFeedTests: XCTestCase {
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(false)
+    }
+
+}
